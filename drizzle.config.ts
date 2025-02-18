0f7455749068ef3da083fe6f55e5e1b93a9b9d3c
Chang <AresChang114@qq.com>
2025-02-18 17:54:24 +0800

    chore: fix drizzle.config type

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,12 +5,13 @@ import 'dotenv/config'
 export default defineConfig({
   out: './src/db/migrations',
   schema: './src/db/schema',
+
   dialect: 'mysql',
   dbCredentials: {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'localhost',
     port: Number.parseInt(process.env.DB_PORT || '3306'),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'mydb',
   },
 })
