03b94078d7c1a7ca189392c555c0a5f4cb4d11cf
Chang <AresChang114@qq.com>
2025-02-22 19:34:50 +0800

    perf: drizzle-zod 模块应用

--- src/dtos/groups.ts
+++ src/dtos/groups.ts
@@ -1,7 +1,6 @@
-import { z } from 'zod'
+import { createInsertSchema } from 'drizzle-zod'
+import { groups } from '~/db/schema/groups'
 
-export const groupsCreateSchema = z.object({
-  name: z.string().min(1, { message: '名称不能为空' }),
-}, {
-  required_error: '参数格式不正确',
+export const groupsCreateSchema = createInsertSchema(groups, {
+  name: schema => schema.nonempty('名称不能为空'),
 })
